ea4972567ec54d329e01eb5943434d7b22708e03
Andrea Dal Ponte <info@andreadalponte.com>
2017-01-18 22:07:04 +0100

    Fix email

--- source/assets/javascripts/blocks/_contacts.js
+++ source/assets/javascripts/blocks/_contacts.js
@@ -1,16 +1,5 @@
 $(function() {
   'use strict';
 
-  // var _init = function(){
-  //   var height = $( window ).height();
-  //
-  //   $('.contacts').css({
-  //     minHeight: height
-  //   });
-  // };
-  //
-  // _init();
-  //
-  // $(window).resize(_init);
-  // $(window).load(_init);
+  $('#contacts-form').attr('action', 'https://formspree.io/info@45-eng.com');
 });
